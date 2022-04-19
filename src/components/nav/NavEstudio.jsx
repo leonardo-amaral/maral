705dd6730cd27b30487caa96105fc5fcf9ca54df
Leonardo Amaral <leozero103@gmail.com>
2022-04-19 13:53:25 -0300

    Nav

--- src/components/nav/NavEstudio.jsx
+++ src/components/nav/NavEstudio.jsx
@@ -8,12 +8,13 @@ function Nav() {
   return (
     <div>
       <header>
-        <nav className='navv'>
+        <nav className='nav'>
           <div className="logo-items">
-            <h1 className="logooo">Maral</h1>
-            <h2 className='foto-logooo'>Fotografias</h2>
+            <h1 className="logoo">Maral</h1>
+            <h2 className='foto-logoo'>Fotografias</h2>
           </div>
           <div className="content-nav">
+            <Link to="/" className='items-nav'>INICIO</Link>
             <Link to="/catalogo" className='items-nav'>CATALÓGO</Link>
             <Link to="/" className='item-sair'>SAIR</Link>
           </div>
